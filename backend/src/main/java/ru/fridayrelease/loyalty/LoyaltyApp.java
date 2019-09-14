a9e5e79439fd860f5a94e579184f6ad993efced2
anthony <antonsdl@gmail.com>
2019-09-14 17:24:14 +0300

    added initial spring-boot-2 app

---
+++ backend/src/main/java/ru/fridayrelease/loyalty/LoyaltyApp.java
@@ -0,0 +1,15 @@
+package ru.fridayrelease.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author avbelyaev
+ */
+@SpringBootApplication
+public class LoyaltyApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyApp.class, args);
+    }
+}
