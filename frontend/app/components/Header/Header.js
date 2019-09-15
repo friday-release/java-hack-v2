98d901e1d2b9a8f726f6c74e9b1dc5ea63701d45
anthony <antonsdl@gmail.com>
2019-09-15 05:07:15 +0300

    adjusted header

--- frontend/app/components/Header/Header.js
+++ frontend/app/components/Header/Header.js
@@ -13,7 +13,7 @@ const useHeaderStyles = makeStyles(theme => ({
   headerTop: {
     backgroundColor: '#ffdb00',
     paddingTop: '30px',
-    height: '100px'
+    height: '50px'
   },
   logo: {
     backgroundColor: 'green'
